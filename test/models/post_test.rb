c8c87a6d0cafe7b72dd705320193d793b7df0f11
Ricardo Hurtado <ricardo.hurtado@trineo.com>
2020-03-10 15:48:06 +1300

    Add post test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @sports_post_1 = posts(:sports_post_1)
+    @sports_post_2 = posts(:sports_post_2)
+    @nutrition_post_1 = posts(:nutrition_post_1)
+    @nutrition_post_3 = posts(:nutrition_post_3)
+    @private_post = posts(:private_post)
+    @sports_admin = users(:sports_admin)
+    @sports_editor = users(:sports_editor)
+    @nutrition_admin = users(:nutrition_admin)
+    @nutrition_editor = users(:nutrition_editor)
+    @private_admin = users(:private_admin)
+    @sports_blog = blogs(:sports)
+    @nutrition_blog = blogs(:nutrition)
+    @private_blog = blogs(:private)
+  end
+
+  test 'belongs_to :user' do
+    assert_equal(@sports_post_1.user, @sports_admin)
+    assert_equal(@sports_post_2.user, @sports_editor)
+    assert_equal(@nutrition_post_1.user, @nutrition_admin)
+    assert_equal(@nutrition_post_3.user, @nutrition_editor)
+    assert_equal(@private_post.user, @private_admin)
+  end
+
+  test 'belongs_to :blog' do
+    assert_equal(@sports_post_1.blog, @sports_blog)
+    assert_equal(@sports_post_2.blog, @sports_blog)
+    assert_equal(@nutrition_post_1.blog, @nutrition_blog)
+    assert_equal(@nutrition_post_3.blog, @nutrition_blog)
+    assert_equal(@private_post.blog, @private_blog)
+  end
 end
